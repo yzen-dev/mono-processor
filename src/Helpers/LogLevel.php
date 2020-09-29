4e28769228b5d4a5d880192d59bcb4d1d5c39f01
Andrey Iatsenko <yzen.dev@gmail.com>
2020-09-29 22:45:56 +0300

    Add:Helpers

---
+++ src/Helpers/LogLevel.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MonoProcessor\Helpers;
+
+use MonoProcessor\Config;
+
+/**
+ * Class LogLevel
+ * @package MonoProcessor\Helpers
+ */
+class LogLevel
+{
+    /**
+     * Checking processor entry for the current level
+     *
+     * @param string $levelName
+     * @return bool
+     */
+    public static function isWrite(string $levelName): bool
+    {
+        return in_array($levelName, Config::getByKey('levels'), true);
+    }
+}
---
+++ src/Helpers/Uuid.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MonoProcessor\Helpers;
+
+/**
+ * Class Uuid
+ * @package MonoProcessor\Helpers
+ */
+class Uuid
+{
+    /**
+     * Formation of a unique identifier V4
+     *
+     * @return string uuid
+     * @static
+     */
+    public static function uuidV4(): string
+    {
+        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0x0fff) | 0x4000,
+            mt_rand(0, 0x3fff) | 0x8000,
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        );
+    }
+}
