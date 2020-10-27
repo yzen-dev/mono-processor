206291864e1d5e1dc307b5d689b5c3adea99c768
Andrey Iatsenko <yzen.dev@gmail.com>
2020-10-27 16:52:38 +0300

    Fix: empty git branch

--- src/Processors/GitInfoProcessor.php
+++ src/Processors/GitInfoProcessor.php
@@ -27,7 +27,7 @@ class GitInfoProcessor
 
         $branches = shell_exec('git branch -v --no-abbrev');
 
-        if (preg_match('{^\* (.+?)\s+([a-f0-9]{40})(?:\s|$)(.*)}m', $branches, $matches)) {
+        if ($branches !== null && preg_match('{^\* (.+?)\s+([a-f0-9]{40})(?:\s|$)(.*)}m', $branches, $matches)) {
             $record['extra']['git'] = [
                 'branch' => $matches[1],
                 'commit' => $matches[2],
